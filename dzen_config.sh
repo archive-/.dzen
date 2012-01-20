7787350f81d7df5165b8f32c76f790c16f521d3c
TJ Koblentz <tj.koblentz@gmail.com>
2012-01-19 22:53:19 -0800

    fixed the widget position and width again (battery)

--- dzen_config.sh
+++ dzen_config.sh
@@ -166,8 +166,8 @@ network() {
 # TODO improve this for less 0% and to know when plugged in better!!
 battery() {
   # X=`expr $X + $W`
-  X=1440
-  W=140
+  X=1420
+  W=180
   LOWBAT=25        # percentage of battery life marked as low
   LOWCOL='#ff4747' # color when battery is low
   while : ; do
